615a9da5650c996a4397fc9dd6856a08069a39dd
nikhilstacks <nikhilstacks@gmail.com>
2022-07-28 23:38:42 +0530

    adding signup and signin get method

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Abe chalrha hai ye to bc...`);
+  res.send(`This is the home page...`);
 });
 
 app.get("/aboutme", (req, res) => {
@@ -10,6 +10,14 @@ app.get("/aboutme", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
+  res.send("this is contact page...");
+});
+
+app.get("/signin", (req, res) => {
+  res.send("this is signin page...");
+});
+
+app.get("/signup", (req, res) => {
   res.send("this is contact page...");
 });
 
