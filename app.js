6422b98c75becc339123329c93e70e4aae4c5543
nikhilstacks <nikhilstacks@gmail.com>
2022-07-28 23:30:20 +0530

    creating server for our application

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`Abe chalrha hai ye to bc...`);
+});
+
+app.get("/aboutme", () => {
+  res.send("this is about me page...");
+});
+
+app.get("/contact", () => {
+  res.send("this is contact page...");
+});
+app.listen(3000, () => {
+  console.log("Server successfully running on port 3000...");
+});
