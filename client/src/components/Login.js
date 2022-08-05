ddfe8ea6a2f616a63388b4a69feab6f72296fc6d
nikhilstacks <nikhilstacks@gmail.com>
2022-08-06 00:21:21 +0530

    login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,9 +2,44 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="pt-5">
-      <p>Welcome</p>
-      <h1>This is my login page..</h1>
+    <div id="login-form-wrap">
+      <h2>Login</h2>
+      <form id="login-form">
+        <p>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            required
+          />
+          <i className="validation">
+            <span></span>
+            <span></span>
+          </i>
+        </p>
+        <p>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email Address"
+            required
+          />
+          <i className="validation">
+            <span></span>
+            <span></span>
+          </i>
+        </p>
+        <p>
+          <input type="submit" id="login" value="Login" />
+        </p>
+      </form>
+      <div id="create-account-wrap">
+        <p>
+          Not a member? <a href="#">Create Account</a>
+        </p>
+      </div>
     </div>
   );
 };
