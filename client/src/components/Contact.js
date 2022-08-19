a472ad94b9cb5b6f33b9dec92ecf0de42ccfe88c
nikhilstacks <nikhilstacks@gmail.com>
2022-08-19 23:52:50 +0530

    sending message into database of the particular user

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [message, setMessage] = useState({
+    message: "",
+  });
+
+  let name, value;
+  const messageValue = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+
+    setMessage({ ...message, [name]: value });
+  };
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { message } = message;
+
+    const res = await fetch("/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: message,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.status === 422 || !data) {
+      console.log("enter something to send");
+    } else {
+      console.log("message send successfully");
+      window.alert("your message has been send successfully");
+    }
+  };
+
   return (
     <div className="pt-5">
       <p>Welcome</p>
       <h1>This is my contact page..</h1>
+      <form method="POST">
+        <label>Message</label>
+        <br></br>
+        <textarea
+          placeholder="Enter your message here"
+          name="message"
+          value={message.message}
+          onChange={messageValue}
+        ></textarea>
+        <br></br>
+        <button type="submit" onClick={sendMessage}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
