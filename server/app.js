460a636448756d9e7358f79ce11d9de197b77211
nikhilstacks <nikhilstacks@gmail.com>
2022-08-22 02:51:06 +0530

    starting adding heroku

--- server/app.js
+++ server/app.js
@@ -32,10 +32,15 @@ app.get("/signup", (req, res) => {
 });
 
 // for heroku
-if (process.env.NODE_ENV == "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
-
 app.listen(PORT, () => {
   console.log(`Server successfully running on port ${PORT}...`);
 });
